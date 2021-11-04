98e2ec491ae2d851248f9fc18b5fc26fc16f218d
Yury Andrea Bonilla Beleno <yuri.bonilla@mercadolibre.com.co>
2021-11-03 22:34:31 -0500

    Progress view of activities

---
+++ app/src/main/java/com/example/notbored/ActivityViewHolder.kt
@@ -0,0 +1,11 @@
+package com.example.notbored
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class ActivityViewHolder(view: View):RecyclerView.ViewHolder(view) {
+
+    fun bind(activityPosition: String) {
+        TODO("Not yet implemented")
+    }
+}
